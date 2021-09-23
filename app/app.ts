876bc617a595c9c27302d566c04e190937af0f88
Ivonne-cas <ivonneximenacaste@gmail.com>
2021-09-22 21:43:40 -0500

    agregue ejercicios de typescript

---
+++ app/app.ts
@@ -0,0 +1,13 @@
+let nombre: string ="ivonne y clelia";
+console.log(`nuestros nombres son ${nombre}`);
+
+//array
+
+let lista:string[]=["riohacha","maicao","dibulla"];
+for (let index=0;index< lista.length; index++){
+    const element =lista[index];
+    console.log(element)
+}
+
+let lista1:number[]=[1,4,6,7,9]
+console.log(lista1);
--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
 "use strict";
+let nombre = "ivonne y clelia";
+console.log(`nuestros nombres son ${nombre}`);
+let lista = ["riohacha", "maicao", "dibulla"];
+for (let index = 0; index < lista.length; index++) {
+    const element = lista[index];
+    console.log(element);
+}
+let lista1 = [1, 4, 6, 7, 9];
+console.log(lista1);
